432745f58718aab715e13ebdb0fff78c32ff2057
Josiah <reikiryo@yahoo.com>
2021-03-21 04:13:28 -0400

    Adds progress bars to About page

--- src/components/About.js
+++ src/components/About.js
@@ -5,11 +5,66 @@ const AboutPage = () => {
         <div className="About">
             <div className="AboutCenter">
                 <div className="AboutCenterLeft">
+
                     <h2>Languages:</h2>
                     <div className="ProgressBar1">
-                        <div></div>
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>Javascript</h3>
+                        </div>
                     </div>
+                    <div className="ProgressBar2">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>Python</h3>
+                        </div>
+                    </div>
+
                     <h2>Technologies:</h2>
+                    <div className="ProgressBar3">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>React</h3>
+                        </div>
+                    </div>
+                    <div className="ProgressBar4">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>Redux</h3>
+                        </div>
+                    </div>
+                    <div className="ProgressBar5">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>HTML</h3>
+                        </div>
+                    </div>
+                    <div className="ProgressBar6">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>CSS</h3>
+                        </div>
+                    </div>
+                    <div className="ProgressBar7">
+                        <div className="BarOuter">
+                            <div className="BarInner"></div>
+                        </div>
+                        <div className="BarText">
+                            <h3>SASS</h3>
+                        </div>
+                    </div>
                 </div>
                 <div className="AboutCenterRight">
                     <h2>I AM A FRONTEND DEVELOPER</h2>
