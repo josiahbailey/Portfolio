fbbd96c382842c0988e578cd3be042125aa69389
Josiah <reikiryo@yahoo.com>
2021-03-22 02:21:46 -0400

    Adjusts centered page content and creates mixins for repetitive styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,10 +5,26 @@ const Contact = () => {
         <div className="Contact">
             <div className="ContactCenter">
                 <div className="ContactCenterLeft">
-
+                    <div className="ContactTitle">
+                        <h2>GET IN TOUCH</h2>
+                    </div>
+                    <div className="ContactText">
+                        <h3>josiahbailey101@yahoo.com</h3>
+                    </div>
+                    <div className="ContactText">
+                        <h3>315-420-6723</h3>
+                    </div>
+                    <div className="ContactText">
+                        <h3>Syrcacuse, New York</h3>
+                    </div>
                 </div>
                 <div className="ContactCenterRight">
-
+                    <form>
+                        <input className="EmailInput" name="email" type="email" placeholder="Email" />
+                        <input className="NameInput" name="name" type="name" placeholder="Name" />
+                        {/* <input className="MessageInput" name="message" type="text" placeholder="Message" /> */}
+                        <textarea className="MessageInput" name="message" placeholder="Message" />
+                    </form>
                 </div>
             </div>
         </div>
